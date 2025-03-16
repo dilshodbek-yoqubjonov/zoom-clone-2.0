3e14baf26f9e0c4c3713443cd47fb8a49db435aa
Yoqubjonov Dilshodbek <yoqubjonovdilshodo4@gmail.com>
2025-03-16 22:52:30 +0500

    Refactor server.js to improve error handling and streamline socket event emissions

--- server.js
+++ server.js
@@ -5,13 +5,12 @@ const { v4: uuidv4 } = require("uuid");
 const io = require("socket.io")(server);
 const { ExpressPeerServer } = require("peer");
 const url = require("url");
-const peerServer = ExpressPeerServer(server, {
-    debug: true,
-});
 const path = require("path");
 
 app.set("view engine", "ejs");
 app.use("/public", express.static(path.join(__dirname, "static")));
+
+const peerServer = ExpressPeerServer(server, { debug: true });
 app.use("/peerjs", peerServer);
 
 app.get("/", (req, res) => {
@@ -19,6 +18,9 @@ app.get("/", (req, res) => {
 });
 
 app.get("/join", (req, res) => {
+    if (!req.query.name) {
+        return res.status(400).send("Ism kiritilmagan!");
+    }
     res.redirect(
         url.format({
             pathname: `/join/${uuidv4()}`,
@@ -28,6 +30,9 @@ app.get("/join", (req, res) => {
 });
 
 app.get("/joinold", (req, res) => {
+    if (!req.query.meeting_id || !req.query.name) {
+        return res.status(400).send("Meeting ID yoki ism kiritilmagan!");
+    }
     res.redirect(
         url.format({
             pathname: req.query.meeting_id,
@@ -43,24 +48,35 @@ app.get("/join/:rooms", (req, res) => {
 io.on("connection", (socket) => {
     socket.on("join-room", (roomId, id, myname) => {
         socket.join(roomId);
-        socket.to(roomId).broadcast.emit("user-connected", id, myname);
+        socket.to(roomId).emit("user-connected", id, myname);
 
         socket.on("messagesend", (message) => {
-            console.log(message);
             io.to(roomId).emit("createMessage", message);
         });
 
         socket.on("tellName", (myname) => {
-            console.log(myname);
-            socket.to(roomId).broadcast.emit("AddName", myname);
+            socket.to(roomId).emit("AddName", myname);
+        });
+
+        socket.on("video-status", (userId, enabled) => {
+            socket.to(roomId).emit("video-status", userId, enabled);
+        });
+
+        socket.on("audio-status", (userId, enabled) => {
+            socket.to(roomId).emit("audio-status", userId, enabled);
         });
 
         socket.on("disconnect", () => {
-            socket.to(roomId).broadcast.emit("user-disconnected", id);
+            socket.to(roomId).emit("user-disconnected", id);
+        });
+
+        socket.on("user-disconnected", (id) => {
+            socket.to(roomId).emit("user-disconnected", id);
         });
     });
 });
 
-server.listen(process.env.PORT || 3030, ()=>{
-    console.log("go to -> http://localhost:3030");
+const PORT = process.env.PORT || 3030;
+server.listen(PORT, () => {
+    console.log(`Server http://localhost:${PORT} da ishga tushdi`);
 });
